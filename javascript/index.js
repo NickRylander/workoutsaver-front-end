ee8ec584f852d195bfb90fcd8ca432d5c64ac69d
NickRylander <nick.rylander@outlook.com>
2020-12-10 16:22:20 -0800

    index html and js

---
+++ javascript/index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+    // whatever has to happen first in application
+    // load in our workouts (get fetch request to backend to load workouts in API)
+    // any event listeners that need to be appended when it loads, append here
+    API.addMovements();
+})
---
+++ javascript/services/API.js
@@ -0,0 +1,13 @@
+class API {
+    // static class level function that is a fetch request to the backend to the index that will load in all my workouts
+    static addMovements(){
+        fetch("http://localhost:3000/workouts")
+        .then(resp => resp.json())
+        .then(workouts => {
+            workouts.forEach(workout => {
+                const{id, workout_name, date, completed, goal, rounds} = workout
+                new Workout(id, workout_name, date, completed, goal, rounds)
+            })
+        })
+    }
+}
